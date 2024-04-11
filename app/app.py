c24951cc71e247d3ffc487d3b41e6c3874970f68
Kyoharo <abdoataa39@gmail.com>
2024-04-11 05:20:49 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,82 @@
+# from flask import (Flask, jsonify)
+
+# app = Flask(__name__)
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def hello():
+#     return jsonify(status=200, message='hello world')
+
+from flask import Flask, request, render_template, Blueprint
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)
+
+class Album:
+    def __init__(self, title, release_year, songs):
+        self.title = title
+        self.release_year = release_year
+        self.songs = songs
+
+class Songer:
+    def __init__(self, name, city, website, biography, albums):
+        self.name = name
+        self.city = city
+        self.website = website
+        self.biography = biography
+        self.albums = albums
+
+def load_data():
+    songers = []
+
+    tree = ET.parse("artists.xml")
+    root = tree.getroot()
+
+    for songer_elem in root.findall("songer"):
+        name = songer_elem.find("name").text
+        city = songer_elem.find("city").text
+        website = songer_elem.find("website").text
+        biography = songer_elem.find("biography").text
+
+        albums = []
+        for album_elem in songer_elem.find("albums").findall("album"):
+            album_title = album_elem.find("title").text
+            release_year = int(album_elem.find("release_year").text)
+
+            songs = [song.text for song in album_elem.find("songs").findall("song")]
+            album = Album(album_title, release_year, songs)
+            albums.append(album)
+
+        songer = Songer(name, city, website, biography, albums)
+        songers.append(songer)
+
+    return songers
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("index.html",
+                           title = "Homepage",
+                           page_head="Artits Search"
+                           )
+
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    q = request.args.get("q")
+    print(q)
+
+    song_data = load_data()
+
+    if q:
+        results = [songer for songer in song_data if q.lower() in songer.name.lower()]
+    else:
+        results = []
+
+    return render_template("search_results.html",
+                            results=results,
+                           custom_css="search_results")
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(debug=True, host='0.0.0.0', port=port)
